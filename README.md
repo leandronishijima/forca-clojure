5409d07aa441e4be5ec9215df50a344686f92e27
leandronishijima <leandro.nishijima@gmail.com>
2016-03-22 20:33:32 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Forca Clojure
+
+Jogo de forca simples, utilizando princípios de programação funcional e a linguagem Clojure
+
+## leiningen project
+
+[Site][leiningen-site]
+
+[leiningen-site]:<http://leiningen.org/>
+
+## Usage
+
+```sh
+$ lein repl
+```
---
+++ src/forca/core.clj
@@ -0,0 +1,48 @@
+(ns forca.core
+    (:gen-class))
+
+(def total-de-vidas 6)
+
+(defn perdeu [] (print "voce perdeu! "))
+
+(defn ganhou [] (print "voce ganhou! "))
+
+(defn letras-faltantes [palavra acertos]
+  (remove (fn [letra] (contains? acertos (str letra))) palavra))
+
+(defn acertou-a-palavra-toda? [palavra acertos]
+  (empty? (letras-faltantes palavra acertos)))
+
+(defn le-letra! [] (read-line))
+
+(defn acertou? [chute palavra]
+  (.contains palavra chute))
+
+(defn imprime-forca [vidas palavra acertos]
+  (println "Vidas" vidas)
+  (doseq [letra (seq palavra)]
+    (if (contains? acertos (str letra))
+      (print letra " ")
+      (print "_" " ")))
+  (println))
+
+(defn jogo [vidas palavra acertos]
+  (imprime-forca vidas palavra acertos)
+  (cond
+    (= vidas 0) (perdeu)
+    (acertou-a-palavra-toda? palavra acertos) (ganhou)
+    :else
+    (let [ chute (le-letra!) ]
+      (if (acertou? chute palavra)
+        (do
+          (println "Voce acertou!")
+          (recur vidas palavra (conj acertos chute)))
+        (do
+          (println "Voce Errou!")
+          (recur (dec vidas) palavra acertos))))))
+
+
+(defn -main
+      "I don't do a whole lot ... yet."
+      [& args]
+      (println "Hello, World!"))
